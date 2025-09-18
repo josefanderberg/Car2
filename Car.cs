9979789cdb25b356d923d8e4acb339fa136ebc4e
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-18 11:12:09 +0200

    Add initial project structure with Car class and solution files

---
+++ Car.cs
@@ -0,0 +1,17 @@
+using System;
+
+internal class Car
+{
+    public string Brand;
+    public string Color;
+    public int MaxSpeed;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Brand:  {Brand}, Color: {Color}, MaxSpeed: {MaxSpeed}");
+    }
+    public void Drive()
+    {
+        Console.WriteLine("The car is driving");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car ferrari = new Car();
+        ferrari.Brand = "Ferrari";
+        ferrari.Color = "Red";
+        ferrari.MaxSpeed = 350;
+
+        Car volvo = new Car();
+        volvo.Brand = "Volvo";
+        volvo.Color = "Blue";
+        volvo.MaxSpeed = 230;
+
+        ferrari.ShowInfo();
+        ferrari.Drive();
+
+        volvo.ShowInfo();
+        volvo.Drive();
+
+    }
+}
